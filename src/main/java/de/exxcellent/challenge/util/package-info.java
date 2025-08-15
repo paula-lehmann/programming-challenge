56b70443aee37fca8ff237be99ef89106b8d2216
paula.lehmann <paula.lehmann@molit.eu>
2025-08-15 20:29:15 +0200

    add packages for weather challenge

---
+++ src/main/java/de/exxcellent/challenge/util/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains utility classes for reading CSV files and analyzing data
+ *
+ * @author paula.lehmann
+ */
+
+package de.exxcellent.challenge.util;
---
+++ src/main/java/de/exxcellent/challenge/weather/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains classes handling weather data
+ *
+ * @author paula.lehmann
+ */
+
+package de.exxcellent.challenge.weather;

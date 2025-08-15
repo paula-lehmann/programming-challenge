4d081f992d175b58b415141af2194ec5eb77199c
paula.lehmann <paula.lehmann@molit.eu>
2025-08-15 20:30:00 +0200

    add stub csv reader

---
+++ src/main/java/de/exxcellent/challenge/util/CsvReader.java
@@ -0,0 +1,19 @@
+package de.exxcellent.challenge.util;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CsvReader {
+
+    /**
+     * Reads csv file and gives content
+     *
+     * @param path of file to be read
+     * @return list of file content
+     * @throws IOException
+     */
+    public List<String[]> readCSV(String path) throws IOException {
+
+        return null;
+    }
+}

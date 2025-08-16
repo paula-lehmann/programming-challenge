960938132cf19d53183779cc9dc182f1d7d715bb
paula.lehmann <paula.lehmann@molit.eu>
2025-08-16 11:59:03 +0200

    add football analyzer methods

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,8 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.football.FootballAnalyzer;
+import de.exxcellent.challenge.weather.WeatherAnalyzer;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -10,16 +13,15 @@ public final class App {
 
     /**
      * This is the main entry method of your program.
+     *
      * @param args The CLI arguments passed
      */
     public static void main(String... args) {
 
-        // Your preparation code …
-
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+        String dayWithSmallestTempSpread = WeatherAnalyzer.findDayWithSmallestSpread("src/main/resources/de/exxcellent/challenge/weather.csv");
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+        String teamWithSmallestGoalSpread = FootballAnalyzer.findTeamWithSmallestDistance("src/main/resources/de/exxcellent/challenge/football.csv");
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
     }
 }
--- src/main/java/de/exxcellent/challenge/football/FootballAnalyzer.java
+++ src/main/java/de/exxcellent/challenge/football/FootballAnalyzer.java
@@ -1,5 +1,46 @@
 package de.exxcellent.challenge.football;
 
+import de.exxcellent.challenge.util.CsvReader;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class FootballAnalyzer {
+
+    /**
+     * Gives team with smallest distance of goals received and scored from file
+     *
+     * @param filePath path to file containing football data to be analyzed
+     * @return team with smallest distance
+     */
+    public static String findTeamWithSmallestDistance(String filePath) {
+        try {
+            List<String[]> csvRows = CsvReader.readCSV(filePath);
+            List<Football> footballData = parseFootballData(csvRows);
+
+            return footballData.stream()
+                    .min(Comparator.comparingInt(Football::getGoalDistance))
+                    .map(team -> String.valueOf(team.teamName()))
+                    .orElse("N/A");
+        } catch (IOException e) {
+            System.out.printf("error while reading csv file: %s", e.getMessage());
+            return "N/A";
+        }
+    }
+
+    /**
+     * converts data to football type
+     *
+     * @param rows List of String containing info of football data
+     * @return List of football data with team name, goals scored and goals received
+     */
+    private static List<Football> parseFootballData(List<String[]> rows) {
+        List<Football> data = new ArrayList<>();
+        for (String[] row : rows) {
+            data.add(new Football(row[0], Integer.parseInt(row[5]), Integer.parseInt(row[6])));
+        }
+        return data;
+    }
 }

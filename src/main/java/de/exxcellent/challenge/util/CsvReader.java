f32953cc2237dd37dc0b875881b70d9f825f9620
paula.lehmann <paula.lehmann@molit.eu>
2025-08-16 11:40:41 +0200

    add method to read csv file

--- src/main/java/de/exxcellent/challenge/util/CsvReader.java
+++ src/main/java/de/exxcellent/challenge/util/CsvReader.java
@@ -1,19 +1,34 @@
 package de.exxcellent.challenge.util;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CsvReader {
 
     /**
-     * Reads csv file and gives content
+     * Reads csv file and gives content, removing file header
      *
      * @param path of file to be read
      * @return list of file content
      * @throws IOException
      */
-    public List<String[]> readCSV(String path) throws IOException {
+    public static List<String[]> readCSV(String path) throws IOException {
+        List<String[]> rows = new ArrayList<>();
 
-        return null;
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String header = br.readLine();
+            if (header == null) {
+                //file is empty
+                return rows;
+            }
+            String line;
+            while ((line = br.readLine()) != null) {
+                rows.add(line.split(","));
+            }
+        }
+        return rows;
     }
 }

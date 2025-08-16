b11f461b373942aca879c55ac279e059af52017b
paula.lehmann <paula.lehmann@molit.eu>
2025-08-16 11:56:55 +0200

    rename variable

--- src/main/java/de/exxcellent/challenge/football/Football.java
+++ src/main/java/de/exxcellent/challenge/football/Football.java
@@ -3,7 +3,7 @@ package de.exxcellent.challenge.football;
 /**
  * Class defines football data object structure with relevant fields
  */
-public record Football(String team, int goals, int goalsAllowed) {
+public record Football(String teamName, int goals, int goalsAllowed) {
     /**
      * Calculates distance between scored goals and received goals against team
      *
